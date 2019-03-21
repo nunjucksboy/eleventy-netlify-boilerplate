689e833a2e336c8a3b9c2dad6075352a46e4f9a0
coey34din <coey34din@gmail.com>
2019-03-21 15:24:22 +0800

    sdfsd

--- _includes/assets/js/navmob.js
+++ _includes/assets/js/navmob.js
@@ -1,6 +1,5 @@
 var burgerBtn = document.getElementById('burgerBtn');
 var mobile = document.getElementById('mobile');
-
 var navmob = document.getElementById('navmob');
 
 burgerBtn.addEventListener('click', function() {
