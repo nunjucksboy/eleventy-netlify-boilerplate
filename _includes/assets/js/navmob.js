79936832319d037e28e6ea73b8139d4014de477b
coey34din <coey34din@gmail.com>
2019-03-21 15:27:33 +0800

    aa

--- _includes/assets/js/navmob.js
+++ _includes/assets/js/navmob.js
@@ -2,6 +2,7 @@ var burgerBtn = document.getElementById('burgerBtn');
 var mobile = document.getElementById('mobile');
 var navmob = document.getElementById('navmob');
 
+
 burgerBtn.addEventListener('click', function() {
   mobile.classList.toggle('navigation');
 }, false);
