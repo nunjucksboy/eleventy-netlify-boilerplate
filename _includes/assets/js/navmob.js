2160a1b22dd950d897f80dedd817fe9a12a5679d
coey34din <coey34din@gmail.com>
2019-03-21 11:22:02 +0800

    sfd

--- _includes/assets/js/navmob.js
+++ _includes/assets/js/navmob.js
@@ -9,8 +9,6 @@ burgerBtn.addEventListener('click', function() {
 
 navmob.addEventListener('click', function() {
   navmob.classList.add('active');
-  demo1.classList.remove('active');
-  demo2.classList.remove('active');
   mobile.classList.add('navmob');
-  mobile.classList.remove('demo1', 'demo2', 'navigation');
+  mobile.classList.remove('navigation');
 }, false);
