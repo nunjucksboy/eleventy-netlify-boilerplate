0ff017df11d5361121d5145878611843faad67db
Nunjucks boy <armandevaroe@gmail.com>
2019-03-27 16:59:40 +0800

    test

--- _includes/assets/js/inline.js
+++ _includes/assets/js/inline.js
@@ -1,10 +1,10 @@
 if (window.netlifyIdentity) {
   window.netlifyIdentity.on("init", user => {
     if (!user) {
-      window.netlifyIdentity.on("login", () => {
-        alert("Ucing baong");
+      window.netlifyIdentity.on("login", () => {        
         document.location.href = "/admin/";
       });
+      alert("Ucing baong");
     }
   });
 }
