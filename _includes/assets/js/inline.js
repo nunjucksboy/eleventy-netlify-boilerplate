fe873996ba1e9f79accdda8725ac3b98a7d0230a
Nunjucks boy <armandevaroe@gmail.com>
2019-03-27 16:16:05 +0800

    test

--- _includes/assets/js/inline.js
+++ _includes/assets/js/inline.js
@@ -16,11 +16,11 @@ netlifyIdentity.init({
 });
  
 netlifyIdentity.open(); // open the modal
-netlifyIdentity.open('login'); // open the modal to the login tab
-netlifyIdentity.open('signup'); // open the modal to the signup tab
+//netlifyIdentity.open('login'); // open the modal to the login tab
+//netlifyIdentity.open('signup'); // open the modal to the signup tab
  
 netlifyIdentity.on('init', user => console.log('init', user));
-netlifyIdentity.on('login', user => console.log('login', user));
+netlifyIdentity.on('login', user => console.log('login heula', user));
 netlifyIdentity.on('logout', () => console.log('Logged out'));
 netlifyIdentity.on('error', err => console.error('Error', err));
 netlifyIdentity.on('open', () => console.log('Widget opened'));
