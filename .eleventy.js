bdb703bacd654a498ca4eb2e51a6e8cae997ec57
Nunjucks boy <armandevaroe@gmail.com>
2019-03-28 14:32:18 +0800

    test

--- .eleventy.js
+++ .eleventy.js
@@ -54,6 +54,7 @@ module.exports = function(eleventyConfig) {
    
   // Don't process folders with static assets e.g. images
   eleventyConfig.addPassthroughCopy("static/img");
+  eleventyConfig.addPassthroughCopy("galery");
   eleventyConfig.addPassthroughCopy("admin");
   eleventyConfig.addPassthroughCopy("_includes/assets/");
 
