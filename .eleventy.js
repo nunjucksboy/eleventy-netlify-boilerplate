446dd9b8d721043513395911626bb379dd13f1d8
Nunjucks boy <armandevaroe@gmail.com>
2019-03-29 17:07:28 +0800

    test article kana image

--- .eleventy.js
+++ .eleventy.js
@@ -78,7 +78,7 @@ module.exports = function(eleventyConfig) {
   // Don't process folders with static assets e.g. images
   eleventyConfig.addPassthroughCopy("static/img");
   eleventyConfig.addPassthroughCopy("galery/img");
-  eleventyConfig.addPassthroughCopy("article/img");
+  eleventyConfig.addPassthroughCopy("articles/img");
   eleventyConfig.addPassthroughCopy("admin");
   eleventyConfig.addPassthroughCopy("_includes/assets/");
 
