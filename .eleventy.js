4366e4959c65662acfc2bae1bf1f34f728be6780
Nunjucks boy <armandevaroe@gmail.com>
2019-03-29 10:25:54 +0800

    test

--- .eleventy.js
+++ .eleventy.js
@@ -92,7 +92,17 @@ module.exports = function(eleventyConfig) {
       data: "_data",
       output: "_site"
     }
-  };
-  
+  }; 
 
 };
+getCatList = function(collection) {
+  let catSet = new Set()
+
+  collection.getAllSorted().forEach(item =>
+        typeof item.data.category === "string"
+    &&  catSet.add(item.data.category))
+
+  return [...catSet]
+}
+
+eleventyConfig.addCollection("categoryList", getCatList)
